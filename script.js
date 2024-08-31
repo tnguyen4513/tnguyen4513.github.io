fb19a90768bb9b2e953832f07b4d8c36c9fd5d2f
Tran Nguyen <98514663+tnguyen4513@users.noreply.github.com>
2024-08-30 22:49:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ document.getElementById('switch').addEventListener('change', function() {
     document.body.classList.toggle('light-mode', this.checked);
     document.querySelector('.mode-label').textContent = this.checked ? 'Light Mode' : 'Dark Mode';
 });
+
+window.onload = function() {
+    var toast = document.getElementById("toast");
+    toast.classList.add("show");
+
+    document.getElementById("closeToast").addEventListener("click", function() {
+        toast.classList.remove("show");
+    });
+
+    // Automatically close the toast after 8 seconds if not closed manually
+    setTimeout(function(){
+        toast.classList.remove("show");
+    }, 8000);
+};
+
